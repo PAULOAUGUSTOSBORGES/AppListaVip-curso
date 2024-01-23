e8b96311830e38536f2af835f2c7537eb14ecaa7
Paulo Augusto Silva Borges <pauloaugusto.silvaborges@outlook.com>
2024-01-23 19:57:37 -0300

    Criei uma classe pessoa e os seus atributos

--- app/src/main/java/devandroid/pauloa/applistavip/view/MainActivity.java
+++ app/src/main/java/devandroid/pauloa/applistavip/view/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import devandroid.pauloa.applistavip.R;
+import devandroid.pauloa.applistavip.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
-
+    Pessoa pessoa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+
+        pessoa.setPrimeiroNome("Paulo Augusto");
+        pessoa.setSobreNome("Silva Borges");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("6299967687");
+
     }
 }
